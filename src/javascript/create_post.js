175d6083e6b228c9cf649bb9b36aba1362201140
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-09-02 09:54:05 -0400

    Users can now use the "View" link after creating a post to view the post

--- src/javascript/create_post.js
+++ src/javascript/create_post.js
@@ -1,6 +1,18 @@
 // create_post.js
 // Functions for creating a new Star post
 
+// Get dialog element and store it in a variable
+var dialog = document.querySelector("dialog");
+// Display dialog polyfill for older browsers that do not support the experimental HTML <dialog> element
+// if (! dialog.showModal) {
+//       dialogPolyfill.registerDialog(dialog);
+// }
+// Add event listener to dialog box close button
+dialog.querySelector(".close").addEventListener("click", function() {
+      // Close dialog box
+      dialog.close();
+});
+
 // Open new post window to create a new post
 const new_post = function () {
       // Reset title text field
@@ -20,8 +32,8 @@ const new_post = function () {
 }
 
 // View post just created by the user (not currently supported)
-const view_post = function () {
-      window.alert("This featured has not yet been added - please check back later.");
+const view_post = function (post_id) {
+      window.location.href = "post.html?id=" + post_id;
 }
 
 // Publish a post and store information in database
@@ -50,7 +62,7 @@ const publish_post = function () {
                   // Message lasts for 5 seconds (5,000 milliseconds)
                   "timeout": 5000,
                   // Call view_post function when "View" link is clicked
-                  "actionHandler": view_post,
+                  "actionHandler": () => view_post(key),
                   "actionText": "View"
             };
             // Display snackbar notification with config information object
@@ -58,20 +70,7 @@ const publish_post = function () {
       }
       //
       else {
-            // Get dialog element and store it in a variable
-            var dialog = document.querySelector("dialog");
-            // Display dialog polyfill for older browsers that do not support the experimental HTML <dialog> element
-            // if (! dialog.showModal) {
-            //       dialogPolyfill.registerDialog(dialog);
-            // }
-
             // Display dialog box
             dialog.showModal();
       }
 }
-
-// Add event listener to dialog box close button
-dialog.querySelector(".close").addEventListener("click", function() {
-      // Close dialog box
-      dialog.close();
-});
