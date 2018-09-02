b35373d35294294f0efac21f43bec2e18e0a5fe4
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-09-01 21:09:06 -0400

    Moved generate_rating_buttons() function from main.js to post_rating.js
    Maintenance!

--- src/javascript/post_rating.js
+++ src/javascript/post_rating.js
@@ -1,6 +1,49 @@
 // post_rating.js
 // Functions related to allowing the user to rate posts and displaying ratings
 
+var colors = [
+      {
+            "red": 24,
+            "green": 93,
+            "blue": 204
+      },
+      {
+            "red": 0,
+            "green": 255,
+            "blue": 0
+      }
+];
+
+const generate_rating_buttons = function (post_id) {
+      var buttons = document.createElement("div");
+
+      for (var i = 0; i < 5; i ++) {
+            button = document.createElement("button");
+            var factor = i / 5;
+            var color = "rgba("
+                   + ((colors[1].red * factor) + (colors[0].red * (1 - factor))) + ", "
+                   + ((colors[1].green * factor) + (colors[0].green * (1 - factor))) + ", "
+                   + ((colors[1].blue * factor) + (colors[0].blue * (1 - factor))) + ", "
+                  + "1)";
+            button.style = "background-color: " + color + " !important; filter: brightness(1"
+            button.className = "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored post-rating-button";
+            button.id = "button-" + (i * 25);
+            // addEventListener cannot be used
+            button.setAttribute("onclick", "rate_post('" + post_id + "', " + (i * 25) + ");");
+
+            if (i == 0) {
+                  button.innerHTML += "<i class='material-icons'>remove</i>";
+            }
+            else if (i == 4) {
+                  button.innerHTML += "<i class='material-icons'>add</i>";
+            }
+
+            buttons.appendChild(button);
+      };
+
+      return buttons;
+}
+
 const rate_post = function (post_id, rating) {
       var user_id = firebase.auth().currentUser.uid;
       database.ref("posts/" + post_id + "/ratings/" + user_id).set({
