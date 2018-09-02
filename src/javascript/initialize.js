dc37345e8c9e8847a8bf669ed38329df6905a2a8
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-09-01 21:08:24 -0400

    Cleaned up Firebase initialization code a little bit

--- src/javascript/initialize.js
+++ src/javascript/initialize.js
@@ -3,12 +3,14 @@
 
 // Initialize Firebase
 var config = {
-      apiKey: "AIzaSyBxupWBF6M4dVv15fZ1Oxf_EDi4FJ5iAzQ",
-      authDomain: "star-5c536.firebaseapp.com",
-      databaseURL: "https://star-5c536.firebaseio.com",
-      projectId: "star-5c536",
-      storageBucket: "star-5c536.appspot.com",
-      messagingSenderId: "139695047939"
+      "apiKey": "AIzaSyBxupWBF6M4dVv15fZ1Oxf_EDi4FJ5iAzQ",
+      "authDomain": "star-5c536.firebaseapp.com",
+      "databaseURL": "https://star-5c536.firebaseio.com",
+      "projectId": "star-5c536",
+      "storageBucket": "star-5c536.appspot.com",
+      "messagingSenderId": "139695047939"
 };
+
 firebase.initializeApp(config);
 var database = firebase.database();
+console.log("Firebase initialized.", config);
