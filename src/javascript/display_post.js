33482bdf2456f033d5baf02b46f61ca169d78458
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-09-07 17:28:42 -0400

    CSS cleanup
    #46

--- src/javascript/display_post.js
+++ src/javascript/display_post.js
@@ -39,36 +39,38 @@ firebase.database().ref("posts/" + post_id).once("value").then(
             var post_ownership;
             firebase.auth().onAuthStateChanged(
                   function (user) {
-                        post_ownership = user.uid == post_info.user_id;
-                        if (post_ownership) {
-                              var delete_post_dialog = document.querySelector("#delete-post-dialog");
-                              // if (! dialog.showModal) {
-                              //       dialogPolyfill.registerDialog(dialog);
-                              // }
-                              delete_post_dialog.querySelector(".close").addEventListener("click", function() {
-                                    delete_post_dialog.close();
-                              });
-                              delete_post_dialog.querySelector(".confirm").addEventListener("click", function() {
-                                    firebase.database().ref("posts/" + post_id).remove();
-                                    delete_post_dialog.close();
-                                    window.location.href = "index.html";
-                              });
+                        if (user) {
+                              post_ownership = user.uid == post_info.user_id;
+                              if (post_ownership) {
+                                    var delete_post_dialog = document.querySelector("#delete-post-dialog");
+                                    // if (! dialog.showModal) {
+                                    //       dialogPolyfill.registerDialog(dialog);
+                                    // }
+                                    delete_post_dialog.querySelector(".close").addEventListener("click", function() {
+                                          delete_post_dialog.close();
+                                    });
+                                    delete_post_dialog.querySelector(".confirm").addEventListener("click", function() {
+                                          firebase.database().ref("posts/" + post_id).remove();
+                                          delete_post_dialog.close();
+                                          window.location.href = "index.html";
+                                    });
 
-                              window.display_delete_post_dialog = function (post) {
-                                    delete_post_dialog.querySelector(".mdl-dialog__title").innerHTML = "Delete Post - " + post.title;
-                                    delete_post_dialog.showModal();
+                                    window.display_delete_post_dialog = function (post) {
+                                          delete_post_dialog.querySelector(".mdl-dialog__title").innerHTML = "Delete Post - " + post.title;
+                                          delete_post_dialog.showModal();
+                                    }
+
+                                    content.innerHTML += '\
+                                          <button id="delete-post-button" class="mdl-button mdl-js-button mdl-button--icon warning" onclick="window.display_delete_post_dialog(post_info)">\
+                                                <i class="material-icons">delete</i>\
+                                          </button>\
+                                          <div class="mdl-tooltip" data-mdl-for="delete-post-button">\
+                                                Delete post\
+                                          </div>\
+                                    ';
+
+                                    componentHandler.upgradeDom();
                               }
-
-                              content.innerHTML += '\
-                                    <button id="delete-post-button" class="mdl-button mdl-js-button mdl-button--icon warning" onclick="window.display_delete_post_dialog(post_info)">\
-                                          <i class="material-icons">delete</i>\
-                                    </button>\
-                                    <div class="mdl-tooltip" data-mdl-for="delete-post-button">\
-                                          Delete post\
-                                    </div>\
-                              ';
-
-                              componentHandler.upgradeDom();
                         }
                   }
             );
