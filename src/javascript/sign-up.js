ff662705194c74e346d7a048b815b90b7cf55fc2
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-09-08 12:36:14 -0400

    Added console logs to sign-up.js
    #49

--- src/javascript/sign-up.js
+++ src/javascript/sign-up.js
@@ -1,28 +1,40 @@
 const create_user = function () {
+      console.log("Creating new user account...");
+      console.log("Getting user information from page...");
       var email = $("#email-input").val().toString();
       var password = $("#password-input").val().toString();
 
+      console.log("Verifying user information...");
       if ((!email || email == "") && (!password || password == "")) {
             error_dialog("Error", "You must enter an e-mail address and a password to create an account.");
+            console.error("E-mail address and password must be provided for account registration.");
       }
       else if (!email || email == "") {
             error_dialog("Error", "You must enter an e-mail address to create an account.");
+            console.error("E-mail address must be provided for account registration.");
       }
       else if (!password || password == "") {
             error_dialog("Error", "You must enter an password to create an account.");
+            console.error("Password must be provided for account registration.");
       }
       else if (password.length < 8) {
             error_dialog("Error", "Password must be at least 8 characters long.");
+            console.error("Password is " + password.length + " characters long. Minimum password length is 8 characters.");
       }
       else if (password.length > 64) {
             error_dialog("Error", "Password must be between 8 and 64 characters long.");
+            console.error("Password is " + password.length + " characters long. Maximum password length is 64 characters.");
       }
+      console.log("User information validated. Creating account...");
       else {
             firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
                   console.log(error.code);
                   console.log(error.message);
             });
-            console.log("Account successfully created.");
+            console.log(
+                  "Account successfully created.",
+                  "Redirecting to home page."
+            );
             window.location.href = "index.html";
       }
 }
